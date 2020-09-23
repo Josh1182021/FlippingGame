d1cbfda4c3abcfdb1a7886ca24e921713389b235
Josh1182021 <66659887+Josh1182021@users.noreply.github.com>
2020-09-22 22:52:05 -0700

    Tuesday night

---
+++ Flipping Platformer/Assets/Scripts/CrystalAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
+        animator.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
